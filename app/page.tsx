87c5a6e8b06704de4cd7e828f64981862b6b9771
Oliver Metz <softwaredevdad@gmail.com>
2026-01-13 22:06:55 +0100

    Add: extra stylings and css to the landing page. Start to implement the navbar.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/ui/landing/nav";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <main className="w-full flex flex-col relative z-10">
+      <Navbar />
+    </main>
+  );
 };
 
 export default Page;
