8e7b43a42e28e34bdfedb3c27b5c7378e06c6b6d
Oliver Metz <softwaredevdad@gmail.com>
2026-01-14 13:51:58 +0100

    Fix: some css styling issues for the layout in our feature page. and finish up the features section for now.

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 
         <Script
           id="unicorn-studio"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `!function(){if(!window.UnicornStudio){window.UnicornStudio={isInitialized:!1};var i=document.createElement("script");i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.29/dist/unicornStudio.umd.js",i.onload=function(){window.UnicornStudio.isInitialized||(UnicornStudio.init(),window.UnicornStudio.isInitialized=!0)},(document.head || document.body).appendChild(i)}}();`,
           }}
